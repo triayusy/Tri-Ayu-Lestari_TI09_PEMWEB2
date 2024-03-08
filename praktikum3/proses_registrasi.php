4b23700c0d505dfcc73bdce8466ea5b30a243b7a
triayusy <triayulestari122@gmail.com>
2024-03-08 10:37:14 +0700

    praktikum 3 - Skills

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,13 @@
+<?php
+//Array Numerik - Index & Value
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+//Array Assosiative - Key & Value
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
+
+
+?>
